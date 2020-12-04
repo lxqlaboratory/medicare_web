18516b8341b2b6727785f1ed008308e6043e74b6
wenxue11 <714876076@qq.com>
2020-12-04 10:47:17 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -66,9 +66,6 @@ export const constantRoutes = [
 
   },
 
-
-
-
   { path: '/', redirect: '/login', hidden: true },
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
